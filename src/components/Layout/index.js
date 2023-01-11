59f078c563fa715a2abe0794e1fe93e7eac1dabf
MargaridaSouto19 <meggysouto@gmail.com>
2023-01-11 12:52:37 +0000

    filters done

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,22 +1,19 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './layout.module.scss'
+import styles from './layout.module.scss';
 import NavBar from '../NavBar';
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.layout}>
       <NavBar />
-      <main>
-        {children}
-      </main>
-      {/* Aqui pode levar o footer */}
+      <main>{children}</main>
     </div>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.element,
-}
+};
 
-export default Layout
+export default Layout;
--- src/components/shared/Filter/index.js
+++ src/components/shared/Filter/index.js
@@ -1,27 +1,57 @@
-import React, { useState } from 'react'
-import styles from './filter.module.scss'
+import React from 'react';
+import styles from './filter.module.scss';
 
-const Filter = ({ filter }) => {
-  const [isActive, setIsActive] = useState(false);
+const Filter = ({
+  filters,
+  currentActive,
+  setCurrentActive,
+  setSelectedFilter,
+}) => {
+  const handleClick = (value) => {
+    setCurrentActive && setCurrentActive(value);
+  };
+  const chooseFilter = (parent, value) => {
+    setSelectedFilter &&
+      setSelectedFilter({
+        type: parent,
+        filter: value,
+      });
+  };
+
   return (
-    <li>
-      <button
-        className={!isActive ? styles.filterBtn : styles.activeFilter}
-        onClick={() => setIsActive(!isActive)}
-      >
-        {filter.type}
-      </button>
-      {
-        isActive && (
-          <ul className={styles.extendedOptions}>
-            {filter.values.map((value, valueIndex) => (
-              <li key={valueIndex}>{value}</li>
-            ))}
-          </ul>
-        )
-      }
-    </li >
-  )
-}
+    <ul className={styles.filterGroup}>
+      {filters.map((item, index) => (
+        <li
+          key={index}
+          className={currentActive === item.type ? styles.activeFilter : ''}
+        >
+          <button
+            className={styles.filterBtn}
+            onClick={() => {
+              handleClick(item.type);
+              currentActive === item.type && setCurrentActive('');
+            }}
+          >
+            {item.type}
+          </button>
+          {currentActive === item.type && (
+            <ul className={styles.extendedOptions}>
+              {item.values.map((value, valueIndex) => (
+                <li key={valueIndex}>
+                  <button
+                    onClick={() => chooseFilter(item.type, value)}
+                    value={value}
+                  >
+                    {value}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default Filter
+export default Filter;
